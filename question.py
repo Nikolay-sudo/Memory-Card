dc9768344cd020fd9730ebaf078bcab8acf6e6f0
Nikolay-sudo <skripnikn334@gmail.com>
2024-12-03 16:22:40 +0200

    Add files via upload

---
+++ question.py
@@ -0,0 +1,13 @@
+questions = [
+    {
+        "question": "Яблуко",
+        "right": "apple",
+        "wrongs": ["application", "building", "caterpillar"]
+    },
+    # Сюди можна додавати інші питання
+    {
+        "question": "Собака",
+        "right": "dog",
+        "wrongs": ["cat", "mouse", "elephant"]
+    }
+]
